ced50e5dfd77495b9cf93498cc9b307052a9cc39
kalkudin <117531275+kalkudin@users.noreply.github.com>
2024-03-24 11:35:12 -0700

    fixed bug with filter

--- app/src/main/java/com/example/finalproject/presentation/screen/owned_stocks/OwnedStocksViewModel.kt
+++ app/src/main/java/com/example/finalproject/presentation/screen/owned_stocks/OwnedStocksViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.finalproject.presentation.screen.owned_stocks
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.finalproject.data.common.ErrorType
@@ -7,9 +8,15 @@ import com.example.finalproject.data.common.Resource
 import com.example.finalproject.domain.usecase.DataStoreUseCases
 import com.example.finalproject.domain.usecase.TransactionsUseCases
 import com.example.finalproject.presentation.event.owned_stocks.OwnedStocksEvent
+import com.example.finalproject.presentation.mapper.company_details.toDomain
+import com.example.finalproject.presentation.mapper.company_details.toPresentation
+import com.example.finalproject.presentation.model.company_details.UserId
 import com.example.finalproject.presentation.model.owned_stocks.OwnedStock
 import com.example.finalproject.presentation.state.owned_stocks.OwnedStocksState
+import com.example.finalproject.presentation.state.watchlisted_stocks.WatchlistedStocksState
 import com.example.finalproject.presentation.util.getErrorMessage
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -63,7 +70,7 @@ class OwnedStocksViewModel @Inject constructor(
                         }
 
                         _ownedStocksState.update { currentState ->
-                            currentState.copy(ownedStocks = ownedStocks)
+                            currentState.copy(ownedStocks = ownedStocks, originalOwnedStocks = ownedStocks)
                         }
                     }
 
@@ -87,13 +94,16 @@ class OwnedStocksViewModel @Inject constructor(
 
     private fun searchOwnedStocks(query: String) {
         viewModelScope.launch {
+            Log.d("OwnedStocksVM", "Search Launched")
             _ownedStocksState.update { currentState ->
                 val originalList = currentState.originalOwnedStocks
+                Log.d("OwnedStocksVM", originalList.toString())
                 val filteredList = if (query.isEmpty()) {
                     originalList
                 } else {
                     originalList?.filter { it.symbol.contains(query, true) }
                 }
+                Log.d("OwnedStocksVM", filteredList.toString())
                 currentState.copy(
                     ownedStocks = filteredList,
                     originalOwnedStocks = currentState.originalOwnedStocks
